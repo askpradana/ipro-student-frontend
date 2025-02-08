6916d2566b5566eeb807d2ffd687b53552a669ea
naufal <askpradana@gmail.com>
2025-02-08 19:55:56 +0700

    feat: add footer on landing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LandingView from '../views/LandingView.vue'
-import SampleChartView from '../views/SampleChartView.vue'
-import DashboardView from '../views/DashboardView.vue'
+import LandingView from '@/views/LandingView.vue'
+import SampleChartView from '@/views/SampleChartView.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
