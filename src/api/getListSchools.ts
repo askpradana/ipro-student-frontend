5dba6ee4ee989037689b88a9498d4f710c298f81
rizalyoga <ach.rizal.yogaswara@gmail.com>
2025-03-05 15:29:55 +0700

    feat: add more students

---
+++ src/api/getListSchools.ts
@@ -0,0 +1,36 @@
+import { useAuthStore } from '@/stores/auth'
+
+interface SchoolListResponse {
+  message: string
+  code: number
+  status: string
+  data: string[]
+}
+
+export const getListSchool = async () => {
+  const authStore = useAuthStore()
+  const token = authStore.user?.token
+
+  try {
+    if (!token) {
+      throw new Error('No user token found')
+    }
+
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/admin/schools`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data: SchoolListResponse = await response.json()
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
